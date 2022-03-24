1aaadf58f2667a5f30c5620d26630d2d5da686e6
Agile <98657990+white12a@users.noreply.github.com>
2022-03-24 20:10:02 +0000

    Update Ans19.java

--- src/Ans19.java
+++ src/Ans19.java
@@ -3,10 +3,12 @@ public class Ans19 {
         int a[]={10,56,78,34,32};
 
         int sum=0;
+        int average=0;
         for(int i=0; i<a.length; i++){
             sum=sum+a[i];
             double average=sum/a.length;
-            System.out.println("average of the array is "+ average);
+            
         }
+         System.out.println("average of the array is "+ average);
     }
 }
